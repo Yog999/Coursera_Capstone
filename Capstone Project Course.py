56438671cdb4a00a9636b179f2c866da27a394ac
Yog999 <55403807+Yog999@users.noreply.github.com>
2019-09-19 16:00:57 -0400

    Add files via upload

---
+++ Capstone Project Course.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# This is for Capstone Project Course
+
+
+# In[3]:
+
+
+import pandas as pd # library for data analsysis
+
+
+# In[4]:
+
+
+import numpy as np # library to handle data in a vectorized manner
+
+
+# In[8]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
